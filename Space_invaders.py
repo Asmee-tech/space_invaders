cea995966c3f94b04d3fd6414ebae69353c685ed
Asmee-tech <asmee.khimani@gmail.com>
2025-12-20 10:56:43 +0100

    Adjust spaceship speed and scaling

--- Space_invaders.py
+++ Space_invaders.py
@@ -13,7 +13,7 @@ hfont=pygame.font.SysFont("lemon drop",30)
 wfont=pygame.font.SysFont("antrokas",50)
 #game constants
 fps=60
-speeds=10
+speeds=2
 speedb=7
 maxb=3
 #loading the spaceships
@@ -21,21 +21,75 @@ red=pygame.image.load("spaceship_red.png")
 blue=pygame.image.load("spaceship_blue.png")
 bg=pygame.image.load("galaxy_bg.jpg")
 bg=pygame.transform.scale(bg,(700,600))
-red=pygame.transform.scale(red,(30,30))
-blue=pygame.transform.scale(blue,(30,30))
+red=pygame.transform.scale(red,(60,60))
+blue=pygame.transform.scale(blue,(60,60))
+#drawing border
+bor=pygame.Rect(350,0,5,600)
 #varieble for health
 blueh=10
 redh=10
+#class for spaceship
+class ss(pygame.sprite.Sprite):
+    def __init__(self,img,x,y):
+        super().__init__()
+        self.image=img
+        self.rect=self.image.get_rect()
+        self.rect.x=x
+        self.rect.y=y
+    #function for vertical movement
+    def vm(self,speed):
+        self.rect.move_ip(0,speed)
+        if self.rect.top<=0 or self.rect.bottom>=600:
+            self.rect.move_ip(0,-speed)
+    #function for horizontal movement
+    def hm(self,speed,p):
+        self.rect.move_ip(speed,0)
+        #p1=red
+        if p==1:
+            if self.rect.left<=0 or self.rect.right>=bor.left:
+                self.rect.move_ip(-speed,0)
+        #p2=blue
+        if p==2:
+            if self.rect.right>=700 or self.rect.left<=bor.right:
+                self.rect.move_ip(-speed,0)
+#creating the spaceship objects
+rso=ss(red,100,300)
+bso=ss(blue,550,300)
+ssg=pygame.sprite.Group()
+ssg.add(rso)
+ssg.add(bso)
 #while loop
 run=True
 while run:
     for event in pygame.event.get():
         if event.type==QUIT:
             run=False
+    keyp=pygame.key.get_pressed()
+    #red movement
+    if keyp[K_w]:
+        rso.vm(-speeds)
+    if keyp[K_a]:
+        rso.hm(-speeds,1)
+    if keyp[K_s]:
+        rso.vm(speeds)
+    if keyp[K_d]:
+        rso.hm(speeds,1)
+    #blue movement
+    if keyp[K_UP]:
+        bso.vm(-speeds)
+    if keyp[K_LEFT]:
+        bso.hm(-speeds,2)
+    if keyp[K_DOWN]:
+        bso.vm(speeds)
+    if keyp[K_RIGHT]:
+        bso.hm(speeds,2)
     screen.blit(bg,(0,0))
+    pygame.draw.rect(screen,"white",bor)
+    #screen.blit(bor,())
     redht=hfont.render("HEALTH:"+str(redh),1,fonth)
     blueht=hfont.render("HEALTH:"+str(blueh),1,fonth)
     screen.blit(redht,(10,20))
-    screen.blit(blueht,(600,20))
+    screen.blit(blueht,(570,20))
+    ssg.draw(screen)
     pygame.display.update()
 pygame.quit()
